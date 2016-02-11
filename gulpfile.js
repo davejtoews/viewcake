38601fadde3e4305a11c1b2c40f979dfad32fdb0
Dave Toews <davejtoews@gmail.com>
2016-02-10 18:26:17 -0700

    Get Browserify working with gulp, and install React

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,11 @@
  
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var browserify = require('browserify');
+var babelify = require('babelify');
+var watchify = require('watchify');
+var debowerify = require('debowerify');
+var source = require('vinyl-source-stream');
  
 gulp.task('sass', function () {
   return gulp.src('./public/sass/**/*.scss')
@@ -12,4 +17,22 @@ gulp.task('sass', function () {
  
 gulp.task('sass:watch', function () {
   gulp.watch('./sass/**/*.scss', ['sass']);
+});
+
+gulp.task('scripts', function() {
+	gulp.src('public/js/main.js')
+		.pipe(browserify({
+			transform: [debowerify, babelify({'presets': ['react']})]
+		}))
+		.pipe(gulp.dest('./public/js'))
+});
+
+gulp.task('buildjs', function () {
+    return browserify({basedir: './public/js/', compact: false})
+        .add("main.js")
+        .transform(babelify, {presets: ['react']})
+        .transform(debowerify)
+        .bundle()
+        .pipe(source('bundle.js'))
+        .pipe(gulp.dest('./public/js/'));
 });
--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,4 @@
 window.Reveal = require("reveal.js");
+
+var React = require('react');
+var ReactDOM = require('react-dom');
